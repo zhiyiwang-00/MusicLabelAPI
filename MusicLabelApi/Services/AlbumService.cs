b456b5ed111330f4f7d1f79f900d8a2e6cf1e820
Dani Tran <dani2009no@gmail.com>
2025-02-21 14:07:41 +0100

    feat: CRUD for album

--- MusicLabelApi/Services/AlbumService.cs
+++ MusicLabelApi/Services/AlbumService.cs
@@ -9,4 +9,40 @@ public class AlbumService{
     {
         _dbcontext = dbcontext;
     }
+
+    public Album GetAlbumById(int id)
+    {
+        var album = _dbcontext.Albums.Find(id);
+        return album;
+    }
+
+    public IEnumerable<Album> GetAllAlbums()
+    {
+        var albums = _dbcontext.Albums.ToList();
+        return albums;
+    }
+
+    public Album CreateNewAlbum(Album album)
+    {
+        _dbcontext.Albums.Add(album);
+        _dbcontext.SaveChanges();
+        return album;
+    }
+
+    public void Update(Album album)
+    {
+        _dbcontext.Albums.Update(album);
+        _dbcontext.SaveChanges();
+    }
+
+    public void Delete(int id)
+    {
+        var album = _dbcontext.Albums.Find(id);
+        if (album == null)
+        {
+            return;
+        }
+        _dbcontext.Albums.Remove(album);
+        _dbcontext.SaveChanges();
+    }
 }
